a21acb153572b219273d1f7300248bb016e9e0bf
zyk1172 <53221608+zyk1172@users.noreply.github.com>
2026-09-17 01:05:38 +0800

    fix(web): install route scroll reset

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import './navigation-scroll-reset'
 import { activateLanguage, initialRequestedLanguage, prepareLanguage } from './i18n'
 import './i18n.profile-overlay.en'
 import './i18n.traffic.en'
